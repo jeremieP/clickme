fcdcee0d249176f3f8db08417ef83b8081ffbc7e
jeremieP <jeremie.pereira@gmail.com>
2022-11-26 22:06:23 +0100

    Update App to manage Clicks and actions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Circle } from './components/Circle'
+import { Button } from './components/Button'
+import { useState } from 'react'
 
 function App() {
+  const [clicks, setClicks] = useState([])
+  const [clickHistory, setClicksHistory] = useState([])
+
+  const onClickHandler = (e) => {
+    // minus 25 because Circle is 50px square + Date.now to have a unique key
+    const clickPosition = { top: e.clientY - 25, left: e.clientX - 25, now: Date.now() }
+    setClicks([...clicks, clickPosition])
+  }
+
+  const undoHandler = (e) => {
+    setClicksHistory([...clickHistory, clicks.pop()])
+    setClicks([...clicks])
+  }
+
+  const redoHandler = (e) => {
+    setClicks([...clicks, clickHistory.pop()])
+    setClicksHistory([...clickHistory])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App" onClick={onClickHandler}>
+        <h1 className="appTitle">Click anywhere !</h1>
+        {
+          clicks.map((el) => (
+            <Circle top={el.top} left={el.left} key={el.now} />
+          ))
+        }
+      </div>
+
+      <div className="actionZone">
+          <Button label="Undo" isActive={clicks.length > 0} onClickHandler={undoHandler} />
+          <Button label="Redo" isActive={clickHistory.length > 0} onClickHandler={redoHandler} />
+        </div>
+    </>
   );
 }
 
